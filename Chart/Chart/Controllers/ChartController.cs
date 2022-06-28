3cf0b6e0e7ee14a78238e87da2a969aba9bdad6c
weichien <ywc92@hotmail.com>
2022-06-29 01:02:21 +0800

    aa

---
+++ Chart/Chart/Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chart.Controllers
+{
+    public class ChartController : Controller
+    {
+        public ActionResult Pie()
+        {
+            return View();
+        }
+
+        public ActionResult Bar()
+        {
+            return View();
+        }
+
+        public ActionResult Line()
+        {
+            return View();
+        }
+
+        public ActionResult MultiChart()
+        {
+            return View();
+        }
+
+        public ActionResult PiePct()
+        {
+            return View();
+        }
+    }
+}
